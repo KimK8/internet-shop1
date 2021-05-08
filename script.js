97ad17730fdc958c59a324b08cb641937d72855b
Ekaterina <xxlbomb@gmail.com>
2021-05-08 16:36:54 +0300

    size-window added

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ $(document).ready(function() {
         $('.header__menu-burger, .header__menu-block').toggleClass('active');
     });
 });
+
+$(document).ready(function() {
+    $('.filter__left').click(function (event) {
+        $('.filter__title, .filter__icon').toggleClass('active');
+    });
+});
+
+$(document).ready(function() {
+    $('.filter__right-item').click(function (event) {
+        $('.filter__size-box').toggleClass('active');
+    });
+});
+

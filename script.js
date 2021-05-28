40aa87cb3308eb957f54af6eedb0f2cece2b30b1
Ekaterina <xxlbomb@gmail.com>
2021-05-28 16:10:58 +0300

    body.lock placement correction

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.header__menu-burger').click(function (event) {
-        $('.header__menu-burger, .header__menu-block').toggleClass('active');
+        $('.header__menu-burger, .header__menu-block, .header__menu, .header__menu__acc, .header__menu__cart').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
 
